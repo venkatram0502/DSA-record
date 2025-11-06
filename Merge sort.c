b82c6b49f8ef995ffca1433620fc988ed5ec7606
venkatram0502 <11249A159@kanchiuniv.ac.in>
2025-11-06 12:15:44 +0530

    Create Merge sort.c

---
+++ Merge sort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+      for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+    // Merge the temp arrays back into arr[left..right]
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+    // Copy remaining elements of L[], if any
+    while (i < n1) {
+        arr[k++] = L[i++];
+    }
+
+    // Copy remaining elements of R[], if any
+    while (j < n2) {
+        arr[k++] = R[j++];
+    }
+}
+// Merge Sort function
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+// Function to display array
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+// Main function
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    printf("\nArray before sorting:\n");
+    display(arr, n);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("\nArray after Merge Sort:\n");
+    display(arr, n);
+
+    return 0;
+}
