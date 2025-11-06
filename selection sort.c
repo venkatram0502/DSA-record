ff79a45b4c6f55010c20fde4d5eaf5aaa0e1c580
venkatram0502 <11249A159@kanchiuniv.ac.in>
2025-11-06 12:08:44 +0530

    Create selection sort.c

---
+++ selection sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+    
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+// Main function
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("\nArray before sorting:\n");
+    display(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("\nArray after Selection Sort:\n");
+    display(arr, n);
+
+    return 0;
+}
